2b81f57a9490caa910b11f918dea4bf6388e9481
Equipolostmary <equipolostmary@gmail.com>
2025-05-21 16:39:19 +0200

    Update panel_punto.py

--- pages/panel_punto.py
+++ pages/panel_punto.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+def mostrar_panel(usuario, promociones, imagenes):
+    st.subheader(f"Área privada de: {usuario['Nombre del punto de venta']}")
+
+    st.write("📈 Promociones actuales:", usuario.get("Nº Promos", 0))
+    st.write("📁 Carpeta Drive asignada:", usuario.get("Carpeta Drive", "No asignada"))
+
+    st.write("Número de promociones nuevas:", promociones)
+
+    if imagenes:
+        st.write("Imágenes seleccionadas:")
+        for img in imagenes:
+            st.image(img, width=200)
+    else:
+        st.info("Aún no se han seleccionado imágenes.")
